f105d3659f53c300b5f01fc547760abf3db76baf
nano <yikai.zeng@tu-dresden.de>
2023-11-03 16:19:27 +0100

    add in task service

--- scripts/landmark2twist.py
+++ scripts/landmark2twist.py
@@ -15,8 +15,7 @@ from acc_controller import acc_pi_control
 class lane_follow:
     def __init__(self):    
         #define topic publisher and subscriber
-        self.bridge = CvBridge()
-        
+        self.bridge = CvBridge()  
         self.test_mode  = bool(rospy.get_param('~test_mode',False))
         self.publish_mask = bool(rospy.get_param('~publish_mask',True))
         self.acc_mode   = bool(rospy.get_param('~acc_on',True))
@@ -62,30 +61,39 @@ class lane_follow:
         self.s_lower_a = int(rospy.get_param('~s_lower_a',130))
         self.v_lower_a = int(rospy.get_param('~v_lower_a',100))
 
-        self.h_upper_a = int(rospy.get_param('~h_upper_a',25))
+        self.h_upper_a = int(rospy.get_param('~h_upper_a',50))
         self.s_upper_a = int(rospy.get_param('~s_upper_a',200))
         self.v_upper_a = int(rospy.get_param('~v_upper_a',200))
         
        # stop_line
         self.h_lower_s = int(rospy.get_param('~h_lower_s',110))
         self.s_lower_s = int(rospy.get_param('~s_lower_s',110))
-        self.v_lower_s = int(rospy.get_param('~v_lower_s',200))
+        self.v_lower_s = int(rospy.get_param('~v_lower_s',170))
 
         self.h_upper_s = int(rospy.get_param('~h_upper_s',150))
         self.s_upper_s = int(rospy.get_param('~s_upper_s',180))
-        self.v_upper_s = int(rospy.get_param('~v_upper_s',255))          
+        self.v_upper_s = int(rospy.get_param('~v_upper_s',235))          
 
         # guide line
-        self.h_lower_g = [0,    150,    75] # through, left , right
-        self.s_lower_g = [55,   55,     25]
-        self.v_lower_g = [170,  145,    185]
+        self.h_lower_g = [5,    150,    10] # through, left , right
+        self.s_lower_g = [80,   40,     20]
+        self.v_lower_g = [160,  135,    190]
         
-        self.h_upper_g = [20,   170,    95]
-        self.s_upper_g = [75,   75,     45]
-        self.v_upper_g = [190,  200,    205]
+        self.h_upper_g = [30,   170,    35]
+        self.s_upper_g = [105,   75,     65]
+        self.v_upper_g = [210,  200,    240]
         
-        self._next_action = 1
-        # fix to left turn now for other subfunction debug
+        # intersection exit
+        self.h_lower_e = 35
+        self.s_lower_e = 50
+        self.v_lower_e = 140
+        
+        self.h_upper_e = 80
+        self.s_upper_e = 100
+        self.v_upper_e = 255
+        
+        self._next_action = 0
+        # fix to right turn now for other subfunction debug
         
         self.stopline_dis  = 0
 
@@ -165,17 +173,17 @@ class lane_follow:
             mask1 = cv2.morphologyEx(mask1,cv2.MORPH_CLOSE,kernel)
             mask2 = cv2.morphologyEx(mask2,cv2.MORPH_CLOSE,kernel)
             
+            stop_lower = np.array([self.h_lower_s,self.s_lower_s,self.v_lower_s])
+            stop_upper = np.array([self.h_upper_s,self.s_upper_s,self.v_upper_s])
+            mask_stop  = cv2.inRange(hsv_image,stop_lower,stop_upper)
+            
         # if test mode,output the center point HSV value
-        kernel = np.ones((9,9),np.uint8)
         if self.acc_mode:
+            kernel = np.ones((9,9),np.uint8)
             line_lower = np.array([self.h_lower_a,self.s_lower_a,self.v_lower_a])
             line_upper = np.array([self.h_upper_a,self.s_upper_a,self.v_upper_a])
             mask = cv2.inRange(hsv_image,line_lower,line_upper)
             mask = cv2.morphologyEx(mask,cv2.MORPH_CLOSE,kernel)
-        
-        stop_lower = np.array([self.h_lower_s,self.s_lower_s,self.v_lower_s])
-        stop_upper = np.array([self.h_upper_s,self.s_upper_s,self.v_upper_s])
-        mask_stop  = cv2.inRange(hsv_image,stop_lower,stop_upper)
 
         width_half  = int(hsv_image.shape[1]/2)
         height_half = int(hsv_image.shape[0]/2)
@@ -183,8 +191,8 @@ class lane_follow:
         if self.test_mode:
             # it print the hsv value of the center point in this image
             # play around here to move the cursor
-            width_select    = width_half + 80
-            height_select   = height_half - 20
+            width_select    = width_half + 18
+            height_select   = height_half + 50
             cv2.circle(res, (width_select ,height_select), 5, (0,0,255), 1)
             cv2.line(res,(width_select -10, height_select), (width_select  +10,height_select), (0,0,255), 1)
             cv2.line(res,(width_select , height_select-10), (width_select , height_select+10), (0,0,255), 1)
@@ -214,10 +222,12 @@ class lane_follow:
                     self.stopline_dis = 0
                 else:
                     self.stopline_dis = int(np.mean(point))
-                if self.stopline_dis >80:
+                # print(self.stopline_dis)
+                if self.stopline_dis > 55:
                     # Approaching the intersection
                     # Switch to line following
                     self._intersection_flag = True
+                    # print('Into an intersection')
             else:
                 # now chose another target
                 lane_lower = np.array([self.h_lower_g[self._next_action],self.s_lower_g[self._next_action],self.v_lower_g[self._next_action]])
@@ -228,7 +238,23 @@ class lane_follow:
                 mask_guide  = cv2.morphologyEx(mask_guide,cv2.MORPH_CLOSE,kernel)               
                 
                 self.center_point = self._search_guide_line(mask_guide,height_half)
-                            
+                
+                # checking the exit of intersection
+                exit_lower = np.array([self.h_lower_e,self.s_lower_e,self.v_lower_e])
+                exit_upper = np.array([self.h_upper_e,self.s_upper_e,self.v_upper_e])
+                
+                mask_exit   = cv2.inRange(hsv_image,exit_lower,exit_upper)
+                point = np.nonzero(mask_exit[height_half:2*height_half,width_half])
+                if len(point[0]) == 0:
+                    exit_dis = 0
+                else:
+                    exit_dis = int(np.mean(point))
+                print(exit_dis)
+                print(point[0])
+                if exit_dis > 15 and len(point[0]) > 5:
+                    self._intersection_flag = False      
+                    #pass
+                
             # ACC function starts here
             if self.acc_mode:
                 i = -40
@@ -265,9 +291,9 @@ class lane_follow:
                 _twist2publish.linear.x = v_x * v_factor
                 _twist2publish.angular.z = w_z * v_factor
                 self.pub_cmd.publish(_twist2publish)
-            else:
-                _twist2publish = Twist()
-                self.pub_cmd.publish(_twist2publish)
+            # else:
+            #     _twist2publish = Twist()
+            #     self.pub_cmd.publish(_twist2publish)
                 
             self.center_point = width_half 
             
@@ -292,11 +318,15 @@ class lane_follow:
                     img_msg = self.bridge.cv2_to_imgmsg(mask_stop, encoding="passthrough")
                     img_msg.header.stamp = rospy.Time.now()
                     self.mask_pub_3.publish(img_msg)
-                else:
+                elif not self.test_mode:
                     img_msg = self.bridge.cv2_to_imgmsg(mask_guide, encoding="passthrough")
                     img_msg.header.stamp = rospy.Time.now()
                     self.mask_pub_1.publish(img_msg)   # reuse the first mask topic for debugging guide line mask
-
+                    
+                    img_msg = self.bridge.cv2_to_imgmsg(mask_exit, encoding="passthrough")
+                    img_msg.header.stamp = rospy.Time.now()
+                    self.mask_pub_3.publish(img_msg)
+                    
                 img_msg = self.bridge.cv2_to_imgmsg(mask, encoding="passthrough")
                 img_msg.header.stamp = rospy.Time.now()
                 self.mask_pub_acc.publish(img_msg)  
@@ -352,10 +382,44 @@ class lane_follow:
                 return (l2+w)/2
             
     def _search_guide_line(self,mask,half_h):
-        for i in range(0,50,10):
+        if self._next_action == 1:
+            range_search = range(20,80,10)
+        elif self._next_action == 2:
+            range_search = range(0,50,10)
+        else:
+            range_search = range(0,50,10)
+        for i in range_search:
             point = np.nonzero(mask[half_h + i])
-            if len(point[0]) > 8:
-                _line_center = int(np.mean(point))
+            #print(len(point[0]))
+            if len(point[0]) > 8 and len(point[0]) < 50: # Remove lines of other directions
+                data = point[0]
+                seg_index = 0
+                seg_dict = {}
+                cur_seg = []
+                for pt in range(len(data)):
+                    if pt == 0:
+                        cur_seg.append(data[pt])
+                    else:
+                        if data[pt] - data[pt-1] < 5:
+                            # continous
+                            cur_seg.append(data[pt])
+                        elif len(cur_seg) > 8:
+                            seg_dict[seg_index] = cur_seg
+                            cur_seg = [data[pt]]
+                            seg_index += 1
+                        else:
+                            cur_seg = [data[pt]]
+                            # too short, abandon
+                if len(cur_seg) > 8:
+                    seg_dict[seg_index] = cur_seg
+                if len(seg_dict) == 0:
+                    return 0
+                if self._next_action == 1:
+                    _line_center = int(np.mean(seg_dict[0]))
+                elif self._next_action == 2 and len(seg_dict) > 1:
+                    _line_center = int(np.mean(seg_dict[1]))
+                else:
+                    _line_center = int(np.mean(seg_dict[0]))
                 return _line_center
         return 0
     
